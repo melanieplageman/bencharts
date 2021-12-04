f4f55fe0fae9e339d41c771830d712ee3e5e6ac8
Melanie Plageman <melanieplageman@gmail.com>
2021-12-04 17:13:51 -0500

    Add basic chart objects
    Basic objects include Chart, Exhibit, and Plot
    Plot is represented by Run
    Add these and start testing grouping methods to get runs into these
    various configurations

---
+++ benchart.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python3
+
+import copy
+from types import SimpleNamespace
+
+class GroupAttributes(SimpleNamespace):
+    pass
+
+class Run:
+    def __init__(self, id, data, metadata):
+        self.id = id
+        self.data = data
+        self.metadata = metadata
+        self.original_metadata = copy.deepcopy(self.metadata)
+
+    def __eq__(self, other):
+        return self.metadata == other.metadata
+
+    def __hash__(self):
+        return hash(str(self.metadata))
+
+    def __repr__(self):
+        return "Run %s: %s\n" % (str(self.id), str(self.original_metadata))
+
+class Chart:
+    def __init__(self, attrs, runs):
+        self.attrs = attrs
+        self.runs = runs
+
+    def __repr__(self):
+        return f'Chart {self.attrs}'
+
+def compare_except_keygen(runs, except_items):
+    dictionary = {}
+    for run in runs:
+        run_string = ''.join(k + str(v) for k, v in run.metadata.items() if k not in except_items)
+        dictionary.setdefault(run_string, []).append(run)
+    return dictionary
+
+class Exhibit:
+    def __init__(self, eid, runs):
+        self.id = eid
+        self.runs = runs
+
+    def __repr__(self):
+        return f'Exhibit {self.id}'
+
+class BenchArt:
+    def __init__(self, runs):
+        self.runs = runs
+        self.exhibit_attrs = []
+        self.chart_attrs = []
+
+    def exhibit(self, key):
+       self.exhibit_attrs.append(key) 
+
+    def chart(self, key):
+        self.chart_attrs.append(key)
+
+    def run(self):
+        exhibit_groups = compare_except_keygen(self.runs, self.exhibit_attrs + self.chart_attrs)
+        
+        exhibits = []
+        all_exhibit_runs = list(exhibit_groups.values())
+        for i in range(len(all_exhibit_runs)):
+            exhibits.append(Exhibit(i,
+                compare_except_keygen(all_exhibit_runs[i], self.chart_attrs)))
+
+        return exhibits
---
+++ run.py
@@ -0,0 +1,66 @@
+from benchart import BenchArt, Run
+import pprint
+
+data = {}
+benchart = BenchArt([
+    Run(1, data, {
+        'machine_id': 223,
+        'machine_os': 'Linux',
+        'machine_location': 'local',
+        'application_version': 12,
+        'application_config_backend_flush_after': 128,
+        'benchmark_name': 'pgbench',
+        'benchmark_workload': 'tpcb'
+        }
+    ),
+    Run(2, data, {
+        'machine_id': 223,
+        'machine_os': 'Linux',
+        'machine_location': 'local',
+        'application_version': 13,
+        'application_config_backend_flush_after': 128,
+        'benchmark_name': 'pgbench',
+        'benchmark_workload': 'tpcb'
+        }
+    ),
+    Run(3, data, {
+        'machine_id': 223,
+        'machine_os': 'Linux',
+        'machine_location': 'local',
+        'application_version': 12,
+        'application_config_backend_flush_after': 256,
+        'benchmark_name': 'pgbench',
+        'benchmark_workload': 'tpcb'
+        }
+    ),
+    Run(4, data, {
+        'machine_id': 223,
+        'machine_os': 'Linux',
+        'machine_location': 'local',
+        'application_version': 12,
+        'application_config_backend_flush_after': 512,
+        'benchmark_name': 'pgbench',
+        'benchmark_workload': 'tpcb'
+        }
+    ),
+    Run(5, data, {
+        'machine_id': 222,
+        'machine_os': 'Linux',
+        'machine_location': 'local',
+        'application_version': 12,
+        'application_config_backend_flush_after': 512,
+        'benchmark_name': 'pgbench',
+        'benchmark_workload': 'tpcb'
+        }
+    ),
+])
+
+benchart.exhibit('application_version')
+benchart.chart('application_config_backend_flush_after')
+
+cohort = benchart.run()
+
+for exhibit in cohort:
+    pprint.pprint(exhibit)
+    pprint.pprint(exhibit.runs)
+
