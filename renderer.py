383d61cb7307b7cb4bceda5ee20de02cbf5b8645
Melanie Plageman <melanieplageman@gmail.com>
2022-08-01 12:05:53 -0400

    Change labeling to allow relabel dictionary
    A bit of a short-term hack to allow relabeling of results. This just
    passes a dictionary all the way down. Perhaps in the future, I can make
    a reverse of the "flattening" done by the loader to get the root
    attribute name

--- renderer.py
+++ renderer.py
@@ -4,14 +4,12 @@ from benchart import Run, RunGroup
 import matplotlib.gridspec as gridspec
 
 class Result:
-    def __init__(self, run, label_components=[]):
+    def __init__(self, run, relabels={}):
         self.run = run
         self.run_id = run.id
         self.df = pd.DataFrame(run.data)
         self.metadata = run.metadata
-        if label_components:
-            string = ', '.join(str(label_component) for label_component in label_components[1:])
-            self.label += f' {string}'
+        self.relabels = relabels
 
     def plot(self, ax):
         self.df.plot(x='ts', y='tps', ax=ax, label=self.metadata['machine_id'])
@@ -22,16 +20,28 @@ class Result:
         show_attrs = self.metadata.keys() - self.run.rungroup.accumulated_attrs
         if not show_attrs:
             return prefix
-        return prefix + ' ' + str(self.metadata.subset(show_attrs))
+        subset = self.metadata.subset(show_attrs)
+
+        result = ''
+        for k, v in subset.items():
+            key = k
+            if k in self.relabels.keys():
+                key = self.relabels[k]
+            result += f'{key}: {v}, '
+
+        return prefix + ': ' + result
+
 
 class Renderer:
-    def __init__(self, figure):
+    def __init__(self, figure, relabels={}):
         self.figure = figure
+        self.relabels = relabels
 
 class GridSpecRenderer(Renderer):
     def __call__(self, renderers, run_group, indent=0):
         renderer, *renderers = renderers
 
+        # TODO: use a javascript library to make this collapsible JSON
         # self.figure.suptitle(run_group)
         gridspec = self.figure.add_gridspec(nrows=len(run_group.children))
         print(" " * indent, f"add_gridspec(nrows={len(run_group.children)})")
@@ -61,28 +71,28 @@ class AxesRenderer(Renderer):
         ax.set_title(str(run_group))
         renderer(renderers, run_group, ax, indent + 2)
 
+
 class PlotRenderer(Renderer):
     def __call__(self, renderers, run_group, ax, indent=0):
-        label_components = []
         results = []
         # TODO: this shouldn't be happening, I think
         if isinstance(run_group, Run):
-            results = [Result(run_group)]
+            results = [Result(run_group, self.relabels)]
 
         elif isinstance(run_group.children[0], Run):
-            results = [Result(run) for run in run_group.children]
+            results = [Result(run, self.relabels) for run in run_group.children]
         else:
-            results = self.flatten(run_group, label_components)
+            results = self.flatten(run_group)
 
         for result in results:
             result.df.plot(x='ts', y='tps', ax=ax, label=result.label)
 
-    def flatten(self, node, label_components):
+    def flatten(self, node):
         if isinstance(node, Run):
-            return [Result(node, label_components)]
+            return [Result(node, self.relabels)]
 
         output = []
         for child in node.children:
-            t = self.flatten(child, label_components + [node])
+            t = self.flatten(child)
             output.extend(t)
         return output
